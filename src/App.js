71d4f6eaeb9f3f1a08ceaf37b102960e76d16cd2
Arunendra Singh <singharunendra978@gmail.com>
2024-02-06 23:25:19 +0530

    feat: Add new component to display question from question option.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import Loader from './components/Loader'
-import Error from './components/Error'
-import StartScreen from './components/StartScreen'
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
 
 // const initialState = {
 //   questions: [],
@@ -15,7 +16,8 @@ function App() {
   // First start with useState hook then switch to useReducer hook.
   const [questions, setQuestions] = useState([]);
   const [status, setStatus] = useState("loading"); // loading, error, ready, active, finished.
-  const totalQuestion = questions.length
+  const totalQuestion = questions.length;
+  const [activeQuestion, setActiveQuestion] = useState(0);
 
   // Load all question using use reducer
   useEffect(() => {
@@ -23,13 +25,13 @@ function App() {
       .then((response) => response.json())
       .then((data) => {
         // Here set the question ans status status with new data.
-        setQuestions(data)
-        setStatus("ready")
+        setQuestions(data);
+        setStatus("ready");
         console.log(data);
       })
       .catch((reason) => {
         console.error("Failed to Fetch.", reason);
-        setStatus("error")
+        setStatus("error");
       });
   }, []);
 
@@ -39,7 +41,16 @@ function App() {
       <Main>
         {status === "error" && <Error />}
         {status === "loading" && <Loader />}
-        {status === "ready" && <StartScreen  totalQuestions={totalQuestion}  />}
+        {status === "ready" && (
+          <StartScreen
+            totalQuestions={totalQuestion}
+            toggleQuizStatus={setStatus}
+          />
+        )}
+        {status === "active" && (
+          <Question questionDetail={questions[activeQuestion]} />
+        )}
+        {/* pass only active question. */}
       </Main>
     </>
   );
